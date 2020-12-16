6911e461773a1625032613ead96ad88bbf20988f
rjousse18 <remi.jousse@free.fr>
2020-12-16 23:27:58 +0100

    First Commit

---
+++ spec/atom-jee-utilities-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomJeeUtilitiesView from '../lib/atom-jee-utilities-view';
+
+describe('AtomJeeUtilitiesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
