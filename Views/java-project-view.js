5943a6db2e2c5689a7c595b4889b0a6b44ded78f
rjousse18 <remi.jousse@free.fr>
2021-01-22 16:15:10 +0100

    - Change some snippets - Add option "new jee listener" - Add option "new jee servlet" - Add option "new jee filter" - rename jf snippet to jfil

---
+++ Views/java-project-view.js
@@ -0,0 +1,27 @@
+'use babel';
+
+export default class MyPackageView {
+    constructor() {
+      // Create root element
+      this.element = document.createElement('div');
+      this.element.classList.add('my-package');
+
+      // Create message element
+      const message = document.createElement('div');
+      message.textContent = 'The MyPackage package is Alive! It\'s ALIVE!';
+      message.classList.add('message');
+      this.element.appendChild(message);
+    }
+    
+    // Returns an object that can be retrieved when package is activated
+    serialize() {}
+
+    // Tear down any state and detach
+    destroy() {
+      this.element.remove();
+    }
+
+    getElement() {
+      return this.element;
+    }
+}
